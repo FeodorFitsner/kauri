3a1ec74bb33b5e58235840162304530c2f559ba4
Stefano Azzalini <stefano.azzalini@yahoo.com>
2015-01-08 10:24:37 +0100

    Add header and tail comments

--- ci/appveyor/build_and_test_msys.sh
+++ ci/appveyor/build_and_test_msys.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+
+##
+##  kauri
+##
+##  Copyright (C) 2015 Stefano Azzalini <steazzalini>
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+
 pwd
 rm -rf build
 mkdir build
@@ -6,3 +26,5 @@ cd build
 cmake -G "MSYS Makefiles" C:\\projects\\kauri
 make
 make check
+
+## END ##
